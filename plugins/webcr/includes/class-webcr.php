0918d5d667d833e896a1378bd7e2538edab21f25
Jai Ranganathan <jai.ranganathan@gmail.com>
2024-03-08 16:25:01 -0800

    Update class-webcr.php

--- plugins/webcr/includes/class-webcr.php
+++ plugins/webcr/includes/class-webcr.php
@@ -215,6 +215,25 @@ class Webcr {
 				return FALSE;
 			}
 
+			//making scene location in Scene custom content type visible to REST API
+		function register_scene_custom_fields() {
+			register_meta(
+				'post', // Object type. In this case, 'post' refers to custom post type 'Scene'
+				'scene_location', // Meta key name
+				array(
+					'show_in_rest' => true, // Make the field available in REST API
+					'single' => true, // Indicates whether the meta key has one single value
+					'type' => 'string', // Data type of the meta value
+					'description' => 'The location of the scene', // Description of the meta key
+					'sanitize_callback' => 'sanitize_text_field', // Callback function to sanitize the value
+					'auth_callback' => function () {
+						return true; // Return true to allow reading, false to disallow writing
+					}
+				)
+			);
+		}
+		add_action('init', 'register_scene_custom_fields');
+
 	}
 
 	/**
