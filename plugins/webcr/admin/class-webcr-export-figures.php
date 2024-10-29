64bee017eeaa1b97c17238241e02dd510cf61c61
superjai <jai.ranganathan@gmail.com>
2024-10-29 00:17:58 -0700

    Update class-webcr-export-figures.php

--- plugins/webcr/admin/class-webcr-export-figures.php
+++ plugins/webcr/admin/class-webcr-export-figures.php
@@ -16,7 +16,6 @@ class Webcr_Export_Figures {
 	 * @since    1.0.0
 	 */
     public function add_export_figures_menu() {
-        if (current_user_can('administrator') || current_user_can('webcr_content_manager')) {
             add_submenu_page(
                 'tools.php',              // Parent slug - adding it under 'Tools'
                 'Export Figures',         // Page title
@@ -25,7 +24,6 @@ class Webcr_Export_Figures {
                 'export-figures',         // Slug (used in the URL)
                 [$this, 'export_figures_page']     // Callback function to output the page content
             );
-        }
     }
 
     // Callback function to display the content of the "Export Figures" page
