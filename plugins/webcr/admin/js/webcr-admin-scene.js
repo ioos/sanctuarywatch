3d39418ec8de92f3936df4ba961083de33875c45
robbiecarroll <47251604+robbiecarroll@users.noreply.github.com>
2025-04-22 13:41:02 -0400

    Changes to text on overview scenes.

--- plugins/webcr/admin/js/webcr-admin-scene.js
+++ plugins/webcr/admin/js/webcr-admin-scene.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		if (titleEl.textContent.trim() === "Overview Scene") {
 			const infoText = document.createElement("div");
 			infoText.className = "exopite-sof-fieldset";
-			infoText.textContent = "For this scene to be set as the overview scene, please save it first, then go to the instance above and set it as the Overview Scene.";
+			infoText.textContent = "For this scene to be set as the overview scene, please select an instance above, save this post, then go to the instance above and set this scene as the Overview Scene.";
 			titleEl.parentNode.insertBefore(infoText, titleEl.nextSibling);
 		}
 	});
